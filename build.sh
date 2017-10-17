ff2515429a9b3df76cadbee6fd0a02b040c293b1
Christian Bernard <cmbernard333@gmail.com>
2017-10-16 21:07:49 -0600

    Added .dockerignore to ignore stdout.conf. updated build.sh to allow os specification and tag listing. moved the original Dockerfile to docker/alpine/Dockfile. created docker/ubuntu/Dockerfile that is based on ubuntu instead of alpine. added 50-default-trim.conf that removes /dev/xconsole. removed rsyslog-mod.conf.

--- build.sh
+++ build.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
+TAGS=()
+for i in "$@"
+do
+case ${i} in
+    -s=*|--operating-system=*)
+    OS="${i#*=}"
+    shift # past argument=value
+    ;;
+    -t=*|--tag=*)
+    TAGS+=("${i#*=}")
+    shift # past argument=value
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+done
+
+# default values
+TAGS+=("-t automox/logdemon:latest")
+
+if [ -z "$OS" ]; then
+	echo "OS not provided. Using ubuntu..."
+	OS="ubuntu"
+fi
+
+# operating system check
+OS=`echo $OS | tr '[:upper:]' '[:lower:]'`
+if [ "$OS" = "ubuntu" ]; then
+	DOCKERFILE=docker/ubuntu/Dockerfile	
+elif [ "$OS" = "alpine" ]; then
+	DOCKERFILE=docker/alpine/Dockerfile
+fi
+
+TAGS_STR=$( IFS=$' '; echo "${TAGS[*]}" )
+
 docker stop logdemon && docker rm logdemon
-docker build --no-cache -t automox/logdemon:latest -f Dockerfile .
-docker run -d --name logdemon automox/logdemon:latest
+docker build --no-cache $TAGS_STR -f "${DOCKERFILE}" .
+# docker run -d --name logdemon automox/logdemon:latest
 
